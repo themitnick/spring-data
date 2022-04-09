825006ece08ffb708e551631c33fc47b6bd25dcb
yaotyrone@gmail.com <yaotyrone@gmail.com>
2022-04-09 13:26:10 +0200

    Add student enity

---
+++ src/main/java/ci/themitnick/springdatajpa/entity/Student.java
@@ -0,0 +1,35 @@
+package ci.themitnick.springdatajpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(
+        uniqueConstraints = @UniqueConstraint(
+                name = "emailUnique",
+                columnNames = "email"
+        )
+)
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentId;
+    private String firstName;
+    private  String lastName;
+    @Column(
+            nullable = false
+    )
+    private String email;
+    private String guardianName;
+    private String guardianEmail;
+    private String gaurdianMobile;
+}
